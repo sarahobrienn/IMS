63d7b5c812580e0fe57b2cc69de6efc5acfa5fc0
jnguyen168 <jenny61650nguyen@gmail.com>
2023-11-10 12:54:30 -0500

    homepage styling

--- src/Webpages/Homepage.js
+++ src/Webpages/Homepage.js
@@ -5,29 +5,50 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div class = "Homepage">
+    <div className="homepage">
       <h1>Inventory Management System</h1>
-      <Link to="/signin">
+
+            <div className="container">
+          
+          <div className="bell">
+            <img src="bell.gif" alt="Notification Icon"/>
+            <Link to="/notification">
+        <button>Notifications Page</button>
+      </Link>
+          </div>
+
+          <div className="summary">
+            <img src="summary.gif" alt="Summary Icon"/>
+            <Link to="/summary">
+        <button>Summary Page</button>
+      </Link>
+          </div>
+
+          <div className="inventory">
+            <img src="inventory.gif" alt="Inventory Icon"/>
+            <Link to="/inventory">
+        <button>Inventory</button>
+      </Link>
+          </div>
+
+          <div className="signin">
+          <img src="signin.gif" alt="Signin Icon"/>
+          <Link to="/signin">
         <button>Sign-in Page</button>
       </Link>
 
-      <div class = "Container">
-      <h2>
-        <p>
-        The Inventory Management System is a feature-rich, user-friendly web application that helps businesses streamline operations, 
-        manage inventory, and make informed decisions. It enables easy product management, automated purchase generation,
-        and real-time sales tracking, ultimately reducing costs and enhancing competitiveness.</p>
-      </h2>
-        <div class = "bell"><img src="bell.gif" alt="Notification Icon"/>
-    </div>
-    <div class = "pic">
-    </div>
-    <div class = "pic">
-    </div>
-    <div class = "pic">
-    </div>
-
-</div>
-</div>
-    );
+        <div className="container">
+          <h2>
+            <p>
+              The Inventory Management System is a feature-rich, user-friendly web application that helps businesses streamline operations, 
+              manage inventory, and make informed decisions. It enables easy product management, automated purchase generation,
+              and real-time sales tracking, ultimately reducing costs and enhancing competitiveness.
+            </p>
+          </h2>
+          <div className="bell">
+            <img src="bell.gif" alt="Notification Icon"/>
+          </div>
+        </div>
+      </div>
+  );
 }
