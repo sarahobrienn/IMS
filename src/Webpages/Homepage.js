70469fe64cc8edca117938bf54941b202ce7d95b
sarahmosouthernco <smobrien@southernco.com>
2023-11-10 18:53:36 -0500

    changes

--- src/Webpages/Homepage.js
+++ src/Webpages/Homepage.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Homepage.css';  // Importing the 'Homepage.css' file
 
@@ -7,48 +6,50 @@ function HomePage() {
     <div className="homepage">
       <h1>Inventory Management System</h1>
 
-            <div className="container">
-          
-          <div className="bell">
-            <img src="bell.gif" alt="Notification Icon"/>
-            <Link to="/notification">
-        <button>Notifications Page</button>
-      </Link>
-          </div>
+      <div className="container">
+        <div className="bell">
+          <img src="bell.gif" alt="Notification Icon"/>
+          <Link to="/notification">
+            <button>Notifications Page</button>
+          </Link>
+        </div>
 
-          <div className="summary">
-            <img src="summary.gif" alt="Summary Icon"/>
-            <Link to="/summary">
-        <button>Summary Page</button>
-      </Link>
-          </div>
+        <div className="summary">
+          <img src="summary.gif" alt="Summary Icon"/>
+          <Link to="/summary">
+            <button>Summary Page</button>
+          </Link>
+        </div>
 
-          <div className="inventory">
-            <img src="inventory.gif" alt="Inventory Icon"/>
-            <Link to="/inventory">
-        <button>Inventory</button>
-      </Link>
-          </div>
+        <div className="inventory">
+          <img src="inventory.gif" alt="Inventory Icon"/>
+          <Link to="/inventory">
+            <button>Inventory</button>
+          </Link>
+        </div>
 
-          <div className="signin">
+        <div className="signin">
           <img src="signin.gif" alt="Signin Icon"/>
           <Link to="/signin">
-        <button>Sign-in Page</button>
-      </Link>
-
-        <div className="container">
-          <h2>
-            <p>
-              The Inventory Management System is a feature-rich, user-friendly web application that helps businesses streamline operations, 
-              manage inventory, and make informed decisions. It enables easy product management, automated purchase generation,
-              and real-time sales tracking, ultimately reducing costs and enhancing competitiveness.
-            </p>
-          </h2>
-          <div className="bell">
-            <img src="bell.gif" alt="Notification Icon"/>
-          </div>
+            <button>Sign-in Page</button>
+          </Link>
         </div>
-      </div>
+      </div> {/* This div closes the container div */}
+
+      <div className="container"> {/* This is a new container div */}
+        <h2>
+          <p>
+            The Inventory Management System is a feature-rich, user-friendly web application that helps businesses streamline operations, 
+            manage inventory, and make informed decisions. It enables easy product management, automated purchase generation,
+            and real-time sales tracking, ultimately reducing costs and enhancing competitiveness.
+          </p>
+        </h2>
+        <div className="bell">
+          <img src="bell.gif" alt="Notification Icon"/>
+        </div>
+      </div> {/* This div closes the second container div */}
+
+    </div> // This div closes the homepage div
   );
 }
 
