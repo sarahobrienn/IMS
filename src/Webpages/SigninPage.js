27a466e9137fd0824fa386ddce4a403f8f7f70e4
sarahmosouthernco <smobrien@southernco.com>
2023-11-24 21:59:18 -0500

    added small changes

--- src/Webpages/SigninPage.js
+++ src/Webpages/SigninPage.js
@@ -5,6 +5,18 @@ import './SigninPage.css';
 function SigninPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = (userType) => {
+    if (
+      (userType === 'admin' && username === 'AdminUser' && password === '12345') ||
+      (userType === 'user' && username === 'sobrien13' && password === '12345')
+    ) {
+      setMessage(`Welcome ${userType === 'admin' ? 'Admin' : 'User'}`);
+    } else {
+      setMessage('Invalid credentials. Please try again.');
+    }
+  };
 
   return (
     <div className="signin-container">
@@ -23,22 +35,29 @@ function SigninPage() {
       </Link>
 
       <div className="login-box">
-        <input 
-          type="text" 
-          placeholder="Username" 
-          className="input-field" 
-          value={username} 
-          onChange={e => setUsername(e.target.value)} 
+        <input
+          type="text"
+          placeholder="Username"
+          className="input-field"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <input 
-          type="password" 
-          placeholder="Password" 
-          className="input-field" 
-          value={password} 
-          onChange={e => setPassword(e.target.value)} 
+        <input
+          type="password"
+          placeholder="Password"
+          className="input-field"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="submit-button">Login</button>
+        <button className="submit-button" onClick={() => handleLogin('admin')}>
+          Login as Admin
+        </button>
+        <button className="submit-button" onClick={() => handleLogin('user')}>
+          Login as User
+        </button>
       </div>
+
+      {message && <p>{message}</p>}
     </div>
   );
 }
